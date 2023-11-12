96e4e5e11867ef2906a6887a92375009d72b1d64
Malik Malope <140521344+libSpace@users.noreply.github.com>
2023-11-12 11:11:13 +0200

    Login function

--- banking_app.py
+++ banking_app.py
@@ -29,6 +29,7 @@ def finish_reg():
             new_file.write(f"Password   : {password} \n")
             new_file.write(f"Age        : {age}\n")
             new_file.write(f"Gender     : {gender}\n")
+            new_file.write(f"Balance    : R0")
             new_file.close()
             notif.config(fg ="green", text="Account has been created succesfully")
         
@@ -71,8 +72,53 @@ def register():
     #register button
     Button(register_screen, text="Register", command=finish_reg, font=("calibri",12)).grid(row= 5, sticky=N, pady = 10)
     
+def login_function():
+    all_accounts = os.listdir()
+    
+    login_name = temp_login_name.get()
+    login_password = temp_login_password.get()
+    if login_name =="" or login_password =="":
+        login_notif.config(fg ="red", text="All fields are required*")
+        return
+    
+    for username in all_accounts:
+        if login_name == username:
+            file = open(username, "r")
+            file_data=file.read()
+            file_data.split("\n")
+            password = file_data[1]
+            #account dash
+            if login_password == password
+            print(file_data)
+            return
+    
 def login():
-    print("This is a login page")
+    #Vars
+    global temp_login_name
+    global temp_login_password
+    global login_notif
+    global login_screen
+    temp_login_name = StringVar()
+    temp_login_password = StringVar()
+    #Login screen
+    login_screen = Toplevel(root)
+    login_screen.title("Login")
+    #Labels
+    Label(login_screen, text="Login to your account", font=("calibri",12)).grid(row=0,sticky=N,pady=10)
+    Label(login_screen, text="Username     ", font=("calibri",12)).grid(row=1,sticky=W)
+    Label(login_screen, text="password ", font=("calibri",12)).grid(row=2,sticky=W)
+    login_notif = Label(login_screen, font=("calibri",12))
+    login_notif.grid(row=4,sticky=N,pady=10)
+    
+    
+    #Entries
+    Entry(login_screen,textvariable=temp_login_name).grid(row=1,column=1,padx=5)#Relative padding
+    Entry(login_screen,textvariable=temp_login_password, show="*").grid(row=2,column=1)
+    
+    #register button
+    Button(login_screen, text="Login", command=login_function, font=("calibri",12), width=16).grid(row= 3, sticky=W, pady = 5)
+    
+    
 
 #Image import
 img = Image.open('Logo.png')
