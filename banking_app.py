cc714c8ae8244ee29ec286b72130c9448b787e5e
Malik Malope <140521344+libSpace@users.noreply.github.com>
2023-11-12 02:08:17 +0200

    Toproot edition

--- banking_app.py
+++ banking_app.py
@@ -6,6 +6,25 @@ from PIL import ImageTk,Image
 root = Tk()
 root.title("Banking app")
 
+#Functions
+def register():
+    '''
+    Creates a popup 
+    window when you 
+    press register button
+    '''
+    register_screen = Toplevel(root)
+    register_screen.title("Register")
+    #Labels
+    Label(register_screen, text="Enter your details to register", font=("calibri",12)).grid(row=0,sticky=N,pady=10)
+    Label(register_screen, text="Name", font=("calibri",12)).grid(row=1,sticky=W,pady=10)
+    Label(register_screen, text="Age", font=("calibri",12)).grid(row=2,sticky=W,pady=10)
+    Label(register_screen, text="Gender", font=("calibri",12)).grid(row=3,sticky=W,pady=10)
+    Label(register_screen, text="password", font=("calibri",12)).grid(row=4,sticky=W,pady=10)
+    
+def login():
+    print("This is a login page")
+
 #Image import
 img = Image.open('Logo.png')
 img = img.resize((150,150))
@@ -19,8 +38,8 @@ Label(root, text ="The most secure bank, CI solutions", font=("arial",14)).grid(
 Label(root,image=img).grid(row=2,sticky=N,pady=15)
 
 #buttons
-Button(root, text="Register", font=("calibri",12),width=20).grid(row=3,sticky=N)
-Button(root, text="Login", font=("calibri",12),width=20).grid(row=4,sticky=N,pady = 5)
+Button(root, text="Register", font=("calibri",12),width=20, command=register).grid(row=3,sticky=N)
+Button(root, text="Login", font=("calibri",12),width=20, command=login).grid(row=4,sticky=N,pady = 5)
 
 
 root.mainloop()
